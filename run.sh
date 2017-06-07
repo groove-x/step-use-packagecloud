09a4ed3d969de40daf47ffe199356d0d54c3bf7c
Ato Araki <ato.araki@groove-x.com>
2017-06-07 14:32:58 +0900

    add unique_id for wercker

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-curl -s https://${WERCKER_USE_PACKAGECLOUD_TOKEN}:@packagecloud.io/install/repositories/${WERCKER_USE_PACKAGECLOUD_REPO_NAME}/script.deb.sh | bash
+curl -s https://${WERCKER_USE_PACKAGECLOUD_TOKEN}:@packagecloud.io/install/repositories/${WERCKER_USE_PACKAGECLOUD_REPO_NAME}/script.deb.sh | unique_id=$WERCKER_USE_PACKAGECLOUD_UNIQUE_ID bash
